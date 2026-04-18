29d0aee45b7ea4238e64d4ee127aea3eb3bc7481
dumeojh <dumeojh@gmail.com>
2026-04-18 22:28:28 +0900

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -1,16 +1,14 @@
-// 파일명: firebase_config.js
-// Firebase 프로젝트 설정 정보 (선생님의 Firebase 콘솔에서 복사해와야 합니다)
+// firebase_config.js
 const firebaseConfig = {
-  apiKey: "선생님의_API_KEY",
-  authDomain: "선생님의_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://선생님의_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "선생님의_PROJECT_ID",
-  storageBucket: "선생님의_PROJECT_ID.appspot.com",
-  messagingSenderId: "선생님의_SENDER_ID",
-  appId: "선생님의_APP_ID"
+    apiKey: "선생님의_API_KEY",
+    authDomain: "선생님의_PROJECT_ID.firebaseapp.com",
+    databaseURL: "https://선생님의_PROJECT_ID-default-rtdb.firebaseio.com",
+    projectId: "선생님의_PROJECT_ID",
+    storageBucket: "선생님의_PROJECT_ID.appspot.com",
+    messagingSenderId: "선생님의_SENDER_ID",
+    appId: "선생님의_APP_ID"
 };
 
-// Firebase 초기화 (시동 걸기)
+// Firebase 초기화
 firebase.initializeApp(firebaseConfig);
-// 실시간 데이터베이스(Realtime Database) 사용 준비
 const db = firebase.database();
